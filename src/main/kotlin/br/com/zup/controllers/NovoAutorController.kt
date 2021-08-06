3531984d28c94984ecf83b6ab1b7fc11f6c26382
GabrielPedricoZup <gabriel.pedrico@zup.com.br>
2021-08-06 16:51:26 -0300

    Validations adicionado

--- src/main/kotlin/br/com/zup/controllers/NovoAutorController.kt
+++ src/main/kotlin/br/com/zup/controllers/NovoAutorController.kt
@@ -4,12 +4,15 @@ import br.com.zup.dtos.NovoAutorRequest
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.validation.Validated
+import javax.validation.Valid
 
+@Validated
 @Controller("/autor")
 class NovoAutorController {
 
     @Post
-    fun cadastraNovoAutor(@Body request : NovoAutorRequest){
+    fun cadastraNovoAutor(@Body @Valid request : NovoAutorRequest){
         println(request)
     }
 }
